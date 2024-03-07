57664962d97490b38d3a38f496465c1868e3c95e
Bruce Hsu <1033684650@qq.com>
2024-03-07 14:52:42 +0800

    add comments to the login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// 登录界面
+// Login Page
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -10,38 +10,50 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
+  // Variables to store the username & password
   String? username;
   String? password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Stack widget allows the overlapping of widgets
       body: Stack(
         children: [
-          ..._background(),
-          _content()
+          ..._background(),       // Background widget
+          _content()              // Content widget
         ],
       )
     );
   }
 
-  // 背景
+  // Background method
   _background() {
     return [
-      Positioned.fill(child: Image.asset("images/login_bg.jpg", fit: BoxFit.cover)),
-      Positioned.fill(child: Container(decoration: const BoxDecoration(color: Colors.black45),)),
+      // Position.fill will expand the child to fill the available space
+      Positioned.fill(
+          child: Image.asset("images/login_bg.jpg", fit: BoxFit.cover)
+      ),
+      // Position.fill with a container to apply a dark overlay on the background image
+      Positioned.fill(
+          child: Container(decoration: const BoxDecoration(color: Colors.black45),)
+      ),
     ];
   }
 
+  // Content method
   _content() {
     return Positioned.fill(
       left: 20,
       right: 20,
-      // 适配小屏幕手机可以支持滚动
+      // ListView provides a scrolling view which is good for small screen devices.
       child: ListView(
         children: const [
           SizedBox(height: 100, width: 1,),
-          Text("账号密码登录", style: TextStyle(fontSize: 26, color: Colors.white),),
+          Text(
+            "账号密码登录",
+            style: TextStyle(fontSize: 26, color: Colors.white),
+          ),
           SizedBox(height: 40,)
         ],
       ),
