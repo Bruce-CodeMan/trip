89f552f57a0a3284027fcaa3f06e9d448b14db2a
Bruce Hsu <1033684650@qq.com>
2024-03-08 17:00:17 +0800

    custom appBar to set the alpha value

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,33 +19,92 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage>
     with AutomaticKeepAliveClientMixin{
 
+  static const appBarScrollOffset = 100;
   final bannerList = [
     "https://www.devio.org/io/flutter_app/img/banner/100h10000000q7ght9352.jpg",
     "https://o.devio.org/images/fa/cat-4098058__340.webp",
     "https://o.devio.org/images/fa/photo-1601513041797-a79a526a521e.webp",
     "https://o.devio.org/images/other/as-cover.png"
   ];
+
+  double appBarAlpha = 0;
   
   // _logoutBtn is a getter that creates an ElevatedButton for logging out.
   get _logoutBtn => ElevatedButton(onPressed: (){
     LoginDao.logout();
   }, child: const Text("退出"));
 
+  get _appBar => Opacity(
+    opacity: appBarAlpha,
+    child: Container(
+      height: 80,
+      decoration: const BoxDecoration(color: Colors.white),
+      child: const Center(
+        child: Padding(
+            padding: EdgeInsets.only(top: 40),
+          child: Text("首页"),
+        ),
+      ),
+    ),
+
+  );
+  
+  get _listView => ListView(
+    children: [
+      BannerWidget(bannerList: bannerList),
+      _logoutBtn,
+      const SizedBox(
+        height: 800,
+        child: ListTile(
+          title: Text("Bruce"),
+        ),
+      )
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      appBar: AppBar(title: const Text("首页"), actions: [
-        _logoutBtn
-      ],),
-      body: Column(
-        children: [BannerWidget(bannerList: bannerList)],
+      body: Stack(
+        children: [
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification){
+                if(scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+                return false;
+              },
+              child: _listView,
+            )
+          ),
+          _appBar
+        ],
       ),
     );
   }
 
   @override
   bool get wantKeepAlive => true;
+
+  void _onScroll(double offset) {
+    print("offset: $offset");
+    double alpha = offset / appBarScrollOffset;
+    if(alpha < 0) {
+      alpha = 0;
+    }else if(alpha > 1) {
+      alpha = 1;
+    }
+    print("alpha: $alpha");
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print("appBarAlpha: $appBarAlpha");
+  }
 }
 
 
